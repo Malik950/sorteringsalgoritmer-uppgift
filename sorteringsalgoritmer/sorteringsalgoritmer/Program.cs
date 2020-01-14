9bdbaea5d5d9c3694ebfc1077efb4dabe430abc6
Malik950 <54435682+Malik950@users.noreply.github.com>
2020-01-14 13:49:35 +0100

    börjat

---
+++ sorteringsalgoritmer/sorteringsalgoritmer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace sorteringsalgoritmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 10, 1000, 100000 };
+            bool flag = true;
+            int temp;
+            int numLenght = number.Lenght;
+
+            for (int i = 1; (i <= (numLength - 1)) && flag; i++)
+            { 
+                flag = false
+                    for (int j = 0; (j < (numLenght - 1); j++)
+
+
+
+
+        }
+    }
+}
